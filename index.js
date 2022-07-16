fde0b7d3739f41cc68d53dc92d89ad82509dccc9
savlana <108239589+savlana@users.noreply.github.com>
2022-07-16 18:14:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const carName ="Mercedes";
+console.log(carName);
+alert(carName)
+
+let maxCarSpeed ="202mph";
+console.log(maxCarSpeed);
+alert(maxCarSpeed);
+
+let carOwner ="Lana";
+console.log(carOwner);
+alert(carOwner);
